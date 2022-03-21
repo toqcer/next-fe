654b0d6cf5e5fd1a4c769a648303ac1b9772722f
toqcer <toqcer@gmail.com>
2022-03-21 15:02:00 +0700

    [FIX] Sidebar full Feature

--- components/atoms/SidebarLink/SidebarLink.js
+++ components/atoms/SidebarLink/SidebarLink.js
@@ -1,14 +1,28 @@
 import Link from 'next/link';
 import { BiChevronRight } from "react-icons/bi";
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import MenuStyle from '/styles/Menu.module.scss'
+import { useRouter } from "next/router";
 
-function SidebarLink({ text, active, href, Icon, children }) {
+function SidebarLink({ text, href, Icon, children }) {
+    const refEl = useRef(null);
+    const router = useRouter();
+    const [isActive, setIsActive] = useState(() => {
+        return router.pathname === href;
+    });
     const [isOpen, setIsOpen] = useState(false);
+    useEffect(() => {
+        try {
+            refEl.current?.querySelector('.active') && setIsOpen(true);
+        } catch (e) {
+            console.error(e);
+        }
+    }, []);
+
     return (
         <>
             {children ? (
-                <li className={`uppercase font-semibold ${active ? 'text-orange active' : 'text-dark-gray '}   rounded-sm list-none flex space-x-2 ${children && "flex-col"} 
+                <li ref={refEl} className={`uppercase font-semibold rounded-sm list-none flex space-x-2 ${children && "flex-col"} 
                  `}>
                     <a className={`${MenuStyle['sidebar-link']} ${isOpen && MenuStyle['dropdown-close']} w-full px-4 py-3 cursor-pointer inline-flex gap-x-2 items-center hover:text-orange hover:bg-gray/30 `} onClick={() => setIsOpen(!isOpen)}>
                         {Icon}
@@ -19,7 +33,7 @@ function SidebarLink({ text, active, href, Icon, children }) {
                 </li>
             ) : (
                 <Link href={href} passHref>
-                    <li className={`${MenuStyle['sidebar-link']} uppercase font-semibold ${active ? 'text-orange active' : 'text-dark-gray '}   rounded-sm list-none flex space-x-2 `}>
+                    <li className={`${MenuStyle['sidebar-link']} uppercase font-semibold ${isActive ? 'active' : 'text-dark-gray '} rounded-sm list-none flex space-x-2 `}>
                         <span className='w-full space-x-2 px-4 py-3 cursor-pointer inline-flex gap-x-2 items-center hover:text-orange hover:bg-gray/30'>{Icon}{text}</span>
                     </li>
                 </Link>
--- components/molecules/Sidebar/Sidebar.js
+++ components/molecules/Sidebar/Sidebar.js
@@ -3,38 +3,98 @@ import { DropdownContainer } from "@components/molecules"
 import { BiDesktop, BiCog, BiKey, BiUser, BiPackage, BiStore } from "react-icons/bi";
 
 function Sidebar() {
+    const sidebarProps = [
+        {
+            border: true,
+            children: [{
+                text: "dashboard",
+                href: "/admin/profile",
+                icon: () => <BiDesktop />,
+            },
+            {
+                text: "profile",
+                href: "/admin/profile",
+                icon: () => <BiUser />,
+            },
+            {
+                text: "Admin",
+                href: "/admin/admin",
+                icon: () => <BiKey />,
+            },
+            ]
+        },
+        {
+            border: true,
+            children: [{
+                text: "product",
+                icon: () => <BiPackage />,
+                children: [
+                    {
+                        text: "profile",
+                        href: "/admin/dashboard",
+                        icon: () => <BiUser />,
+                    },
+                    {
+                        text: "Admin",
+                        href: "/admin/admin",
+                        icon: () => <BiKey />,
+                    }
+                ]
+            },
+            {
+                text: "marketplace",
+                href: "/admin/marketplace",
+                icon: () => <BiStore />
+            },]
+        }
 
+    ]
     return (
         <aside className="max-w-xs w-full py-8 px-6 h-full overflow-y-scroll">
             <h1 className="text-4xl font-bold text-orange text-center pb-12 border-b-2 border-muted">ToqCer</h1>
             <div className="select-none">
-                <SidebarSection border>
+                {sidebarProps.map((sidebarProp, index) => (
+                    <SidebarSection key={index} border={sidebarProp?.border}>
+                        {sidebarProp.children.map(level1 => (
+                            level1?.children ?
+                                (<SidebarLink key={Math.random} text={level1.text} href={level1.href} Icon={level1.icon()}>
+                                    <DropdownContainer key={Math.random()}>
+                                        {level1.children.map(level2 => (
+                                            <SidebarLink text={level2.text} key={Math.random()} href={level2.href} Icon={level2.icon()} />
+                                        ))}
+                                    </DropdownContainer>
+                                </SidebarLink>) : (
+                                    <SidebarLink text={level1.text} key={Math.random()} href={level1.href} Icon={level1.icon()} />
+                                ))
+                        )}
+                    </SidebarSection>)
+                )}
+                {/* <SidebarSection border>
                     <SidebarLink text="dashboard" href='#1' Icon={<BiDesktop />} active />
                     <SidebarLink text="setting" href='#2' Icon={<BiCog />} />
                     <SidebarLink text="admin" href='#3' Icon={<BiKey />} />
                     <SidebarLink text="user" href='#4' Icon={<BiUser />}>
                         <DropdownContainer>
-                            <SidebarLink text="Dshbor" href='#4' Icon={<BiUser />} active />
-                            <SidebarLink text="SSSS" href='#4' Icon={<BiUser />} />
+                            <SidebarLink text="Dshbor" href='#4' Icon={<BiUser />} />
                         </DropdownContainer>
                     </SidebarLink>
                 </SidebarSection>
                 <SidebarSection border>
                     <SidebarLink text="product" href='#4' Icon={<BiPackage />}>
                         <DropdownContainer>
-                            <SidebarLink text="product" href='#4' Icon={<BiUser />} active />
+                            <SidebarLink text="product" href='#4' Icon={<BiUser />} />
                         </DropdownContainer>
                     </SidebarLink>
                     <SidebarLink text="marketplace" href='#4' Icon={<BiStore />}>
                         <DropdownContainer>
-                            <SidebarLink text="product" href='#4' Icon={<BiUser />} active />
+                            <SidebarLink text="product" href='#4' Icon={<BiUser />} />
                         </DropdownContainer>
                     </SidebarLink>
                 </SidebarSection>
                 <SidebarSection>
                     <SidebarLink text="dashboard" href='#1' Icon={<BiDesktop />} />
                     <SidebarLink text="setting" href='#2' Icon={<BiCog />} />
-                </SidebarSection>
+                </SidebarSection> */}
             </div>
         </aside >
     )
