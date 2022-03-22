eb99dceefb5d18c5a7df7858e9749492088b8dfc
BariqDharmawan <dharmawan.muhammad03@gmail.com>
2022-03-22 12:03:20 +0700

    [FIX] tailwind config

--- components/atoms/SidebarLink/SidebarLink.js
+++ components/atoms/SidebarLink/SidebarLink.js
@@ -24,7 +24,7 @@ function SidebarLink({ text, href, Icon, children }) {
             {children ? (
                 <li ref={refEl} className={`uppercase font-semibold text-dark-gray rounded-sm list-none flex space-x-2 ${children && "flex-col"} 
                  `}>
-                    <a className={`${MenuStyle['sidebar-link']} ${isOpen && MenuStyle['dropdown-close']} w-full px-4 py-3 cursor-pointer inline-flex gap-x-2 items-center hover:text-orange hover:bg-gray/30 `} onClick={() => setIsOpen(!isOpen)}>
+                    <a className={`${MenuStyle['sidebar-link']} ${isOpen && MenuStyle['dropdown-close']} w-full px-4 py-3 cursor-pointer inline-flex gap-x-2 items-center hover:text-orange hover:bg-gay/30 `} onClick={() => setIsOpen(!isOpen)}>
                         {Icon}
                         {text}
                         <span className={`${MenuStyle['arrow-dropdown']} transition-transform duration-300 ml-auto`}><BiChevronRight /></span>
@@ -34,7 +34,7 @@ function SidebarLink({ text, href, Icon, children }) {
             ) : (
                 <Link href={href} passHref>
                     <li className={`${MenuStyle['sidebar-link']} uppercase font-semibold ${isActive ? 'active' : 'text-dark-gray '} rounded-sm list-none flex space-x-2 `}>
-                        <span className='w-full space-x-2 px-4 py-3 cursor-pointer inline-flex gap-x-2 items-center hover:text-orange hover:bg-gray/30'>{Icon}{text}</span>
+                        <span className='w-full space-x-2 px-4 py-3 cursor-pointer inline-flex gap-x-2 items-center hover:text-orange hover:bg-gay/30'>{Icon}{text}</span>
                     </li>
                 </Link>
             )
--- pages/admin/profile.js
+++ pages/admin/profile.js
@@ -1,21 +1,16 @@
-import { Gap } from "@components/atoms"
 import { Header, Sidebar } from "@components/molecules"
 
-function Dashboard() {
+const ProfileAdmin = () => {
     return (
         <div className="flex h-screen relative">
             <Sidebar />
             <main className="flex-1 overflow-y-scroll h-screen">
                 <section className="bg-primary px-14">
-                    <Header title='dashboard' />
-                    <div>
-
-                    </div>
+                    <Header title='Profile' />
                 </section>
-                <Gap height={1000} />
             </main>
         </div>
-    )
+    );
 }
-
-export default Dashboard
+ 
+export default ProfileAdmin;
