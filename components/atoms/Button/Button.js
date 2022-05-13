e4f43357a05ab047719556897260d7d9cca1c46f
BariqDharmawan <dharmawan.muhammad03@gmail.com>
2022-05-14 04:11:13 +0700

    [UPDATE] trigger confirm password if two of them filled

--- components/atoms/Button/Button.js
+++ components/atoms/Button/Button.js
@@ -1,9 +1,13 @@
-function Button({ children, className, ...rest }) {
+function Button({ children, className, disabled, ...rest }) {
     return (
-        <button className={`uppercase w-full font-bold bg-orange py-2 px-2 hover:shadow-md hover:bg-amber-500 ${className}`} {...rest}>
+        <button
+            className={`uppercase w-full font-bold bg-orange py-2 px-2 hover:shadow-md hover:bg-amber-500 ${className} 
+            ${disabled ? "opacity-50" : ""}`}
+            {...rest}
+        >
             {children}
         </button>
-    )
+    );
 }
 
-export default Button
+export default Button;
