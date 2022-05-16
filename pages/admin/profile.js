abe97427a2979b2e2c8ec44b0cbf98d2d811b113
BariqDharmawan <dharmawan.muhammad03@gmail.com>
2022-05-17 02:20:07 +0700

    [UPDATE] profile consume dummy API

--- pages/admin/profile.js
+++ pages/admin/profile.js
@@ -4,10 +4,10 @@ import { useRouter } from "next/router";
 import Image from "next/image";
 import Cookie from "js-cookie";
 import {
-  BiBarChart,
-  BiPieChart,
-  BiUserPlus,
-  BiBadgeCheck,
+    BiBarChart,
+    BiPieChart,
+    BiUserPlus,
+    BiBadgeCheck,
 } from "react-icons/bi";
 
 import { Button, Input } from "@components/atoms";
@@ -15,85 +15,97 @@ import { DashboardCard } from "@components/molecules";
 import AdminTemplates from "@components/templates/admin/AdminTemplates";
 
 const ProfileAdmin = (props) => {
-  const [data, setData] = useState("");
-  const router = useRouter();
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [data, setData] = useState({ name: "", email: "" });
+    const router = useRouter();
 
-  useEffect(async () => {
-    const token = Cookie.get("tokenAdmin");
-    try {
-      const response = await axios.get(
-        "https://staging-api.toqcer.uloy.dev/v1/admin/summary",
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
+    useEffect(async () => {
+        const token = Cookie.get("tokenAdmin");
+        try {
+            //dummy api
+            const response = await axios.get(
+                "https://jsonplaceholder.typicode.com/users/1",
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+            const data = response.data;
+            setData(data);
+
+            console.log(data);
+        } catch (e) {
+            const status = e.response?.status;
+            if (status === 403) {
+                router.reload();
+            }
         }
-      );
-      const data = response.data.data;
-      setData(data);
-      // setTimeout(() => {
-      // 	setIsLoading(false);
-      // }, 2000);
-    } catch (e) {
-      const status = e.response?.status;
-      if (status === 403) {
-        router.reload();
-      }
-    }
-  }, []);
+    }, []);
 
-  return (
-    <AdminTemplates title="Profile">
-      
-      <div className="mt-12 ">
-        <div className="min-w[400px] flex w-[70%] justify-between bg-midnight-blue text-white p-5 rounded-lg">
-          <div className="flex-1 mb-5">
-            <div className="font-bold text-lg">
-              <h3 className="capitalize ">my account</h3>
-              <span className="uppercase text-muted mt-2">
-                admin information
-              </span>
-            </div>
-            <div className="mt-6">
-              <div>
-                <Input
-                  label="name"
-                  labelColor="text-white"
-                  placeholder="Nama"
-                />
-              </div>
-              <div className="flex gap-x-2 mt-4">
-                <div className="flex-1">
-                  <Input
-                    label="Email"
-                    type="email"
-                    labelColor="text-white"
-                    placeholder="Email"
-                  />
+    useEffect(() => {
+        setName(data.name);
+        setEmail(data.email);
+    }, [data]);
+
+    return (
+        <AdminTemplates title="Profile">
+            <div className="mt-12 ">
+                <div className="min-w[400px] flex w-[70%] justify-between bg-midnight-blue text-white p-5 rounded-lg">
+                    <div className="flex-1 mb-5">
+                        <div className="font-bold text-lg">
+                            <h3 className="capitalize ">my account</h3>
+                            <span className="uppercase text-muted mt-2">
+                                admin information
+                            </span>
+                        </div>
+                        <div className="mt-6">
+                            <div>
+                                <Input
+                                    label="name"
+                                    name="name"
+                                    labelColor="text-white"
+                                    placeholder="Nama"
+                                    onChange={(e) => setName(e.target.value)}
+                                    value={name}
+                                />
+                            </div>
+                            <div className="flex gap-x-2 mt-4">
+                                <div className="flex-1">
+                                    <Input
+                                        label="Email"
+                                        type="email"
+                                        name="email"
+                                        labelColor="text-white"
+                                        placeholder="Email"
+                                        onChange={(e) =>
+                                            setEmail(e.target.email)
+                                        }
+                                        value={email}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="py-4 max-w-[160px]">
+                            <Button className="text-black h-12 rounded-md">
+                                Edit
+                            </Button>
+                        </div>
+                    </div>
+                    <div className="w-1/3 relative">
+                        <div className="absolute top-1/2 w-[320px] h-full -translate-y-1/2 left-14">
+                            <Image
+                                src="/dummy/avatar.svg"
+                                layout="fill"
+                                alt="avatarpict"
+                            />
+                        </div>
+                    </div>
                 </div>
-                <div className="flex-1">
-                  <Input
-                    label="password"
-                    type="password"
-                    labelColor="text-white"
-                    placeholder="Password"
-                  />
-                </div>
-              </div>
             </div>
-            <div className="py-4 max-w-[160px]">
-              <Button className="text-black h-12 rounded-md">Edit</Button>
-            </div>
-          </div>
-          <div className="w-1/3 relative">
-            <div className="absolute top-1/2 w-[320px] h-full -translate-y-1/2 left-14">
-              <Image src="/dummy/avatar.svg" layout="fill" alt="avatarpict" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </AdminTemplates>
-  );
+        </AdminTemplates>
+    );
 };
 
 export default ProfileAdmin;
